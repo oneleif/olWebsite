9aadaf706764b852003f023d4669609a213488dd
zmeriksen <zmeriksen@gmail.com>
2019-10-19 14:53:45 -0500

    WIP Added upload and get for images

--- Sources/App/Controllers/ImageController.swift
+++ Sources/App/Controllers/ImageController.swift
@@ -5,4 +5,41 @@
 //  Created by Zach Eriksen on 10/19/19.
 //
 
-import Foundation
+import Vapor
+import Imperial
+import Authentication
+
+struct ImageController: RouteCollection {
+    let imageFolder = "ProfilePictures/"
+    
+    func boot(router: Router) throws {
+        let authSessionRouter = router.grouped(User.authSessionsMiddleware())
+        
+        authSessionRouter.post("uploadImage", use: uploadImageHandler)
+        authSessionRouter.get("image", ImageUpload.parameter, use: getImageHandler)
+    }
+    
+    func uploadImageHandler(_ req: Request) throws -> Future<ImageUploadResponse> {
+        let user = try req.requireAuthenticated(User.self)
+        return try req.content
+            .decode(ImageUpload.self)
+            .flatMap { imageData in
+                let workPath = try req.make(DirectoryConfig.self).workDir
+                let name = try "\(user.requireID())-\(UUID().uuidString).jpg"
+                let path = workPath + self.imageFolder + name
+                FileManager().createFile(atPath: path,
+                                         contents: imageData.picture,
+                                         attributes: nil)
+                
+                return Future.map(on: req) { ImageUploadResponse(fileName: name) }
+        }
+    }
+    
+    func getImageHandler(_ req: Request) throws -> Future<Response> {
+        let imageName = try req.parameters.next(ImageUpload.self)
+        
+        let path = try req.make(DirectoryConfig.self).workDir + self.imageFolder + imageName
+        
+        return try req.streamFile(at: path)
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,6 +6,9 @@ public func routes(_ router: Router) throws {
     let usersController = UserController()
     try router.register(collection: usersController)
     
+    let imageController = ImageController()
+    try router.register(collection: imageController)
+    
     let imperialController = ImperialController()
     try router.register(collection: imperialController)
 }
