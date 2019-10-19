227a216814deb851daf91f13149a9a4df139cccb
zmeriksen <zmeriksen@gmail.com>
2019-10-19 14:52:58 -0500

    Added Image Models

--- Sources/App/Models/ImageUpload.swift
+++ Sources/App/Models/ImageUpload.swift
@@ -5,4 +5,21 @@
 //  Created by Zach Eriksen on 10/19/19.
 //
 
-import Foundation
+import Vapor
+
+struct ImageUpload: Content {
+    var picture: Data
+}
+
+struct ImageUploadResponse: Content {
+    let fileName: String?
+}
+
+extension ImageUpload: Parameter {
+    typealias ResolvedParameter = String
+    
+    static func resolveParameter(_ parameter: String,
+                                 on container: Container) throws -> ImageUpload.ResolvedParameter {
+        return parameter
+    }
+}
