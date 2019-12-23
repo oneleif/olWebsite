642d832fdf29f02bd84b5fcc4695ea10213b7561
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2019-12-23 14:15:00 +0100

    Update register user handler

--- Sources/App/Controllers/API/UserController.swift
+++ Sources/App/Controllers/API/UserController.swift
@@ -10,9 +10,14 @@ import FluentSQL
 import Crypto
 import Authentication
 
+enum UserValidationError: Error {
+    case usernameAlreadyTaken
+}
+
 class UserController: RouteCollection {
     func boot(router: Router) throws {
-        router.post("api", "register", use: register)
+        
+        router.post(RegisterUserRequest.self, at: "api", "register", use: self.register)
         
         let authSessionRouter = router.grouped(User.authSessionsMiddleware())
         authSessionRouter.post("api", "login", use: login)
@@ -22,33 +27,55 @@ class UserController: RouteCollection {
     
     // MARK: Request Handlers
     
-    func register(_ req: Request) throws -> Future<HTTPStatus> {
-        return try req.content.decode(User.self).flatMap { user in
-            return User.query(on: req).filter(\User.username == user.username).first().flatMap { result in
-                if let _ = result {
-                    return Future.map(on: req) {
-                        return .badRequest
-                    }
+    func register(_ req: Request, _ registerBody: RegisterUserRequest) throws -> Future<HTTPResponse> {
+        return User.query(on: req)
+            .filter(\User.username == registerBody.username)
+            .first()
+            .flatMap { existingUser -> Future<User> in
+                guard existingUser == nil else {
+                    throw UserValidationError.usernameAlreadyTaken
                 }
-                user.password = try BCryptDigest().hash(user.password)
-
-                return user.save(on: req).flatMap { user in
-                    user.social = SocialInformation(id: user.id,
-                                                username: user.username,
-                                                firstName: "",
-                                                lastName: "",
-                                                email: "",
-                                                discordUsername: "",
-                                                githubUsername: "",
-                                                tags: [],
-                                                profileImage: "",
-                                                biography: "",
-                                                links: [],
-                                                location: "")
-                    return user.save(on: req).map { _ in
-                        return .accepted
-                    }
-                }
+                
+            let hashedPassword = try BCryptDigest().hash(registerBody.password)
+            let user = User(username: registerBody.username, password: hashedPassword)
+                
+                return Future.map(on: req) { user }
+        }.flatMap { user in
+            return user.save(on: req)
+        }.flatMap { (user: User) -> EventLoopFuture<User> in
+            user.social = SocialInformation(id: user.id,
+                                            username: user.username,
+                                            firstName: "",
+                                            lastName: "",
+                                            email: "",
+                                            discordUsername: "",
+                                            githubUsername: "",
+                                            tags: [],
+                                            profileImage: "",
+                                            biography: "",
+                                            links: [],
+                                            location: "")
+            
+            return user.save(on: req)
+        }.map(to: PublicUserResponse.self) { user in
+            PublicUserResponse(id: user.id, username: user.username, social: user.social)
+        }.map(to: HTTPResponse.self) { publicUser in
+            var response = HTTPResponse(status: .ok)
+            try JSONEncoder().encode(publicUser, to: &response, on: req)
+            return response
+        }.mapIfError { error -> HTTPResponse in
+            guard let error = error as? UserValidationError else {
+                return HTTPResponse(status: .internalServerError)
+            }
+            
+            switch error {
+            case .usernameAlreadyTaken:
+                let errorBody = [
+                    "message": "Username already taken"
+                ]
+                var response = HTTPResponse(status: .badRequest)
+                try? JSONEncoder().encode(errorBody, to: &response, on: req)
+                return response
             }
         }
     }
