97c35f81f95651222d632c1fa865c4f9e7aa1238
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2020-01-09 00:48:03 +0100

    Add tests for ignore/force functions

--- Tests/AppTests/Services/ValidatorServiceTests.swift
+++ Tests/AppTests/Services/ValidatorServiceTests.swift
@@ -12,12 +12,23 @@ struct DummyValidatable: ValidatableContent {
     }
 }
 
+struct EvenDummierValidatable: ValidatableContent {
+    var test: String
+    
+    static func validations() throws -> Validations<EvenDummierValidatable> {
+        var validations = Validations(EvenDummierValidatable.self)
+        try validations.add(\.test, .count(3...))
+        return validations
+    }
+}
+
 
 class ValidatorServiceTests: XCTestCase {
     private var validator: ValidatorService! = nil
     
     var invalidDummy: DummyValidatable! = nil
     var validDummy: DummyValidatable! = nil
+    var invalidDummier: EvenDummierValidatable! = nil
     
     let tooShortTest = "a"
     let okTest = "aaaa"
@@ -25,6 +36,9 @@ class ValidatorServiceTests: XCTestCase {
     override func setUp() {
         self.invalidDummy = DummyValidatable(test: self.tooShortTest)
         self.validDummy = DummyValidatable(test: self.okTest)
+        
+        self.invalidDummier = EvenDummierValidatable(test: self.tooShortTest)
+        
         self.validator = ValidatorService()
     }
     
@@ -33,7 +47,7 @@ class ValidatorServiceTests: XCTestCase {
         XCTAssertNoThrow(try self.validator.validate(self.validDummy))
     }
     
-    func testShouldSkipValidation() throws {
+    func testShouldSkipAllValidations() throws {
         self.validator.skipValidation = true
         
         XCTAssertNoThrow(try self.validator.validate(self.validDummy))
@@ -45,5 +59,20 @@ class ValidatorServiceTests: XCTestCase {
         
         XCTAssertThrowsError(try self.validator.validate(self.invalidDummy))
         XCTAssertNoThrow(try self.validator.validate(self.validDummy))
+    }
+    
+    func testShouldValidateForcedEvenWhenValidationIsSkipped() throws {
+        self.validator.skipValidation = true
+        self.validator.forceValidation(of: EvenDummierValidatable.self)
+        
+        XCTAssertThrowsError(try self.validator.validate(self.invalidDummier))
+        XCTAssertNoThrow(try self.validator.validate(self.invalidDummy))
+    }
+    
+    func testShouldNotValidateIgnored() throws {
+        self.validator.ignoreValidation(of: EvenDummierValidatable.self)
+        
+        XCTAssertThrowsError(try self.validator.validate(self.invalidDummy))
+        XCTAssertNoThrow(try self.validator.validate(self.invalidDummier))
     }
 }
