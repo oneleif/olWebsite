966697708f1c0aa6643ed9570fbecbeef900a140
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2020-01-09 00:00:25 +0100

    Add validate extension to Request

--- Sources/App/Services/ValidatorService.swift
+++ Sources/App/Services/ValidatorService.swift
@@ -2,6 +2,10 @@ import Vapor
 
 typealias ValidatableContent = Content & Validatable & Reflectable
 
+enum ValidatorServiceError: Error {
+    case validatorNotRegistered
+}
+
 struct ValidatorService: Service {
     var skipValidation: Bool = false
     
