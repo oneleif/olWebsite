2405ef5a5bf56b65ed141b0957e9cf413558f186
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2020-02-07 22:00:11 +0100

    Add files for deploying app on AWS with CodeDeploy

---
+++ Deploy/hooks/after.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# clean up
+CONTAINER_NAME=oneleif-api-$DEPLOYMENT_GROUP_NAME
+IMAGE_FILE=$(find . -name "oneleif-api.*.tar.gz" | head -n 1)
+
+case $DEPLOYMENT_GROUP_NAME in
+  "production")
+    PORT=80
+    ;;
+  "staging")
+    PORT=8080
+    ;;
+esac
+
+echo "Deploying oneleif-api ($DEPLOYMENT_GROUP_NAME) on port $PORT..."
+
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+docker load -i $IMAGE_FILE
+
+docker create -p $PORT:80 --name $CONTAINER_NAME oneleif-api
+docker start $CONTAINER_NAME
---
+++ Deploy/hooks/validate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+case $DEPLOYMENT_GROUP_NAME in
+  "production")
+    PORT=80
+    ;;
+  "staging")
+    PORT=8080
+    ;;
+esac
+
+# wait for application start on $PORT
+while ! nc -z localhost $PORT </dev/null; do sleep 10; done
+
+HEALTH_CHECK_URL=localhost:$PORT/docs/index.html
+
+STATUS_CODE=$(curl --write-out %{http_code} --silent --output /dev/null $HEALTH_CHECK_URL)
+
+if [[ "$STATUS_CODE" -ne 200 ]] ; then
+  echo "Status code is not 200 ($STATUS_CODE), failed deployment"
+  exit 1
+else
+  exit 0
+fi
