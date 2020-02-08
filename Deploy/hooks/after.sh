ea47b5626c0caf4aa1dd444198081723db2f56cb
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2020-02-08 01:59:42 +0100

    Add some comments to scripts

--- Deploy/hooks/after.sh
+++ Deploy/hooks/after.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# clean up
 CONTAINER_NAME=oneleif-api-$DEPLOYMENT_GROUP_NAME
 IMAGE_FILE=$(find .. -name "oneleif-api.*.tar.gz" | head -n 1)
 
@@ -18,10 +17,12 @@ esac
 
 echo "Deploying oneleif-api ($DEPLOYMENT_GROUP_NAME) on port $PORT..."
 
+# clean up
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
+# load revision image
 docker load -i $IMAGE_FILE
 
-docker create -p $PORT:80 --name $CONTAINER_NAME oneleif-api
-docker start $CONTAINER_NAME
+# start new version
+docker run --restart=on-failure:10 --name $CONTAINER_NAME -p $PORT:80 -d oneleif-api
