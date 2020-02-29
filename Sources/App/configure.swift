ce3082e185430a30ed222b6a271e17d216a8fdee
Zach Eriksen <zmeriksen@icloud.com>
2020-02-29 14:32:26 -0600

    Updated psql config to use Environment variables

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -32,7 +32,12 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     // Configure a PostgreSQL database
     let config = PostgreSQLDatabaseConfig(hostname: "localhost", port: 5432, username: "postgres", database: "website", transport: .cleartext)
-
+    let config = PostgreSQLDatabaseConfig(hostname: Environment.get("DB_HOST") ?? "localhost", 
+                                          port: Int(Environment.get("DB_PORT") ?? "") ?? 5432, 
+                                          username: Environment.get("DB_USER") ?? "oneleif", 
+                                          database: Environment.get("DB_NAME") ?? "oneleif-\(env.name)", 
+                                          password: Environment.get("DB_PASSWORD") ?? "root", 
+                                          transport: .cleartext)
 
     let postgres = PostgreSQLDatabase(config: config)
     
