eb5801b420417eb8a2fcf95e920e02f6d74d468b
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2020-03-13 01:47:46 +0100

    Add method for refreshing access token

--- Sources/App/Services/AuthService.swift
+++ Sources/App/Services/AuthService.swift
@@ -6,9 +6,34 @@ class AuthService: Service {
     func createAccessToken(for user: User, on database: DatabaseConnectable) throws -> Future<AccessTokenResponse> {
         let accessToken = try TokenHelpers.createAccessToken(from: user)
         let expiredAt = try TokenHelpers.expiredDate(of: accessToken)
+        let refreshToken = TokenHelpers.createRefreshToken()
+        let accessTokenResponse = AccessTokenResponse(accessToken: accessToken, refreshToken: refreshToken, expiredAt: expiredAt)
         
-        let accessTokenResponse = AccessTokenResponse(accessToken: accessToken, expiredAt: expiredAt)
+        return RefreshToken(token: refreshToken, userId: try user.requireID())
+            .save(on: database)
+            .transform(to: accessTokenResponse)
+    }
+    
+    func refreshAccessToken(refreshToken: String, on request: Request) throws -> Future<AccessTokenResponse> {
+        let refreshTokenModel = RefreshToken.query(on: request)
+            .filter(\.token == refreshToken)
+            .first()
+            .unwrap(or: Abort(.unauthorized))
         
-        return database.future(accessTokenResponse)
+        return refreshTokenModel.flatMap { refreshTokenModel in
+            if refreshTokenModel.expiredAt > Date() {
+                return refreshTokenModel.user.get(on: request)
+                    .flatMap { user in
+                        return try self.createAccessToken(for: user, on: request)
+                }.then { accessToken in
+                    return refreshTokenModel.delete(on: request)
+                        .transform(to: accessToken)
+                }
+            } else {
+                return refreshTokenModel.delete(on: request).thenThrowing {
+                    throw Abort(.unauthorized)
+                }
+            }
+        }
     }
 }
