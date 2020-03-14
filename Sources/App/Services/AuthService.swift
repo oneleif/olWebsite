675d3f63cbb471d2363b93ab68a4c170e411f4b0
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2020-03-14 20:47:25 +0100

    Add documentation for methods in AuthService

--- Sources/App/Services/AuthService.swift
+++ Sources/App/Services/AuthService.swift
@@ -3,14 +3,22 @@ import FluentPostgreSQL
 import JWT
 
 class AuthService: Service {
-    func createAccessToken(for user: User, on database: DatabaseConnectable) throws -> Future<AccessTokenResponse> {
+    /// Creates new access and refresh token pair for logged in user
+    ///
+    /// Both access and refresh tokens are stored in the database.
+    /// The stored values are used for further refreshing of the token and for invalidating them (logout).
+    ///
+    /// - Parameters:
+    ///   - user: instance of user model that is logging in
+    ///   - request: request instance
+    func createAccessToken(for user: User, on request: Request) throws -> Future<AccessTokenResponse> {
         let accessToken = try TokenHelpers.createAccessToken(from: user)
         let accessTokenValue = try TokenHelpers.getAccessTokenValue(fromPayloadOf: accessToken)
         let expiresAt = try TokenHelpers.expiredDate(of: accessToken)
         let refreshToken = TokenHelpers.createRefreshToken()
         let accessTokenResponse = AccessTokenResponse(accessToken: accessToken, refreshToken: refreshToken, expiresAt: expiresAt)
         
-        return database.transaction(on: .psql) { (database) -> EventLoopFuture<AccessTokenResponse> in
+        return request.transaction(on: .psql) { (database) -> EventLoopFuture<AccessTokenResponse> in
             return RefreshToken(token: refreshToken, userId: try user.requireID())
                 .save(on: database)
                 .flatMap { refreshTokenModel in
@@ -23,6 +31,17 @@ class AuthService: Service {
         }
     }
     
+    
+    /// Refreshes access token by creating new pair of access and refresh token for the same user.
+    ///
+    /// If provided refresh token is already expired, it throws Unauthorized error.
+    ///
+    /// Refreshed access token and the provided refresh token are removed from the database
+    /// to prevent users from using the same refrehs token again and again.
+    ///
+    /// - Parameters:
+    ///   - refreshToken: refresh token assigned to access token which has to be refreshed
+    ///   - request: request instance
     func refreshAccessToken(refreshToken: String, on request: Request) throws -> Future<AccessTokenResponse> {
         let refreshTokenModel = RefreshToken.query(on: request)
             .filter(\.token == refreshToken)
@@ -55,12 +74,26 @@ class AuthService: Service {
         }
     }
     
+    
+    /// Returns AccessToken model from database which has the same value as provided in parameter
+    /// - Parameters:
+    ///   - value: value to find
+    ///   - request: request instance
     func findAccessToken(value: String, on request: Request) throws -> Future<AccessToken?> {
         return AccessToken.query(on: request)
             .filter(\.value == value)
             .first()
     }
     
+    
+    /// Removes access token and associated refresh token from database.
+    ///
+    /// Removing the tokens from database prevents using the same access/refresh token
+    /// after the user logged out.
+    ///
+    /// - Parameters:
+    ///   - token: Bearer token sent by client
+    ///   - request: request instance
     func invalidateToken(_ token: String, on request: Request) throws -> Future<Void> {
         let accessToken = try TokenHelpers.verifyToken(token)
         
